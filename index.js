1593f8d392d2761990a45ff35ac6130bb3e43432
TrevorAThomas5 <TrevorAThomas5@gmail.com>
2021-04-14 02:14:43 -0700

    done except scrollbar

--- index.js
+++ index.js
@@ -19,33 +19,6 @@ window.addEventListener('scroll', function(event) {
         text = document.getElementById('date-text');
         text.style.opacity = '0';
     }, 1000);
-
-
-    /*
-    // line animations
-    var lines = document.querySelectorAll('img, video');
-    for(var i = 0; i < lines.length; i++) {
-        var wHeight = window.innerHeight;
-        var scrollY = window.scrollY || window.pageYOffset;
-        var scrollPosition = scrollY + wHeight;
-        var elementPosition = lines[i].getBoundingClientRect().top + scrollY + lines[i].clientHeight;
-
-        if(scrollPosition > elementPosition) {
-            // run animation
-            lines[i].style.transform = 'scale(1)';
-            lines[i].style.boxShadow = 'rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px';
-            lines[i].style.opacity = '1';
-        }
-        else {
-            lines[i].style.transform = 'scale(1.2)';
-            lines[i].style.boxShadow = 'rgba(0, 0, 0, 0.3) 0px 19px 38px, rgba(0, 0, 0, 0.22) 0px 15px 12px';
-            lines[i].style.opacity = '0';
-        }   
-        
-    }
-    */
-
-
 }, false );
 
 
